67409177ed30847a5f4facf38c00c3bc661ae374
sivanagarajuparimisetty58 <sivanagarajuparimisetty58@gmail.com>
2025-08-09 20:19:54 +0530

    updated

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ixuszdrnotgbvrugehvy.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4dXN6ZHJub3RnYnZydWdlaHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2MjcwMDgsImV4cCI6MjA3MDIwMzAwOH0.e7fG85OdpYxk8Npu76Tn99pVLxLuAAiARHWvF_yIkCQ"
+const supabaseUrl = "https://neclltgwsfknlzaehkln.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5lY2xsdGd3c2Zrbmx6YWVoa2xuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NDg0MzUsImV4cCI6MjA3MDMyNDQzNX0.QgrBr-3grYXUPkFYk_MrFh4pdwBFMGKUzs0sWvNYN6Q"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
